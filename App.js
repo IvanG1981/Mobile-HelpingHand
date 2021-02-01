6c0ce64c13c4aee1c9cac1052193679686e734db
Ivan Gonzalez <gonzalez.ivand@gmail.com>
2021-02-01 15:43:23 -0500

    Add Image component

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!</Text>
+    <View style={ styles.container }>
+      <Text style={ { paddingBottom: 50 }}>Hello World!!</Text>
+      <Image         
+        source={ require('./images/community.png') }
+        style={ styles.mainImage }
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +20,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems:'center',
+    justifyContent:'center'
   },
+  mainImage: {
+    width: 100,
+    height: 100
+  }
 });
